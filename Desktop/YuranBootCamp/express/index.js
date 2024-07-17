757090b907b90ea83c6b4010266c12a9dbeb90c6
yuran-nhassengo <yurannhassengo@gmail.com>
2024-07-17 15:59:49 +0200

    Add newroutes

--- Desktop/YuranBootCamp/express/index.js
+++ Desktop/YuranBootCamp/express/index.js
@@ -4,14 +4,23 @@ const port = 3000;
 const app = express()
 
 app.get("/", (req,res) =>{
-    res.type("text/plain");
-    res.send("Hello Express")
+    res.type("text/html");
+    res.send("<h1>Hello Express</h1>")
+});
+
+app.get("/about", (req,res) =>{
+    res.type("text/html");
+    res.send(`<h1>Ola,${req.query.name}</h1>`)
+});
+
+app.use((req,res)=>{
+    res.type("text/html");
+    res.status(404);
+    res.send("<h1> Pagina nao encontrada</h1>")
 })
 
 
-
-
 app.listen(port,()=>{
-    console.log("Servidor iniciado");
+    console.log(` Local http://localhost${port}`);
 });
 
